3ab4f5968cb1d6fb54d6339e4c5aa42e7afa8c85
weijie.yuan <weijie.yuan@aispeech.com>
2016-11-22 19:34:12 +0800

    cut audio data

--- channel_convert.c
+++ channel_convert.c
@@ -7,14 +7,14 @@
 
 /*
  * Usage: channel_convert input_channel(s) input_file output_channel(s) output_file
- *
- * Function: cut audio data(cut from tail) and synthetise (one to multi,max is 16)
+ * Function: cut audio data(cut from tail)
  */
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include"buffer.h"
+#include"config.h"
 
 int main(int argc, const char *argv[])
 {
@@ -27,25 +27,26 @@ int main(int argc, const char *argv[])
     
     int raw = atoi(argv[1]);
     int new = atoi(argv[3]);
-    if (raw < 0 || new < 0 || raw > 16 || new > 16) //channel(s) between 0 and 16
+    if (raw <= 0 || new <= 0 || raw > 16 || new > 16) //channel(s) between 0 and 16
     {
         fprintf(stderr,"Error: channel(s) must be positive number and max channels is 16.\n");
         exit(EXIT_FAILURE);
     }
     
     /* judge channel */
-    if (raw >= new)
+    if (raw <= new)
     {
-        printf("Action: cut audio data\n");
-    }
-    else if(raw != 1)
-    {
-        fprintf(stderr,"Error: only one channel data can be synthetised to multichannel audio data.\n");
+        fprintf(stderr,"Error: input_channel(s) must more than output_channel(s).\n");
         exit(EXIT_FAILURE);
+    }
+    else if(raw == 1)
+    {
+        fprintf(stderr,"NOTE: input_channel(s) must more than one,exit.\n");
+        exit(EXIT_SUCCESS);
     }
     else
     {
-        printf("Action: synthetise audio data\n");
+        printf("Action: cut audio data\n");
     }
 
     /* open file */
@@ -62,9 +63,8 @@ int main(int argc, const char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    int dot_len = sizeof(short int);
-    short int *in_buf = (short int *)audio_calloc(raw,dot_len);
-    short int *out_buf = (short int *)audio_calloc(new,dot_len);
+    short int *in_buf = (short int *)audio_calloc(raw,LENGTH);
+    short int *out_buf = (short int *)audio_calloc(new,LENGTH);
     
     printf("Running message:\n  input_file:%s\n  input_channel(s):%d\n  output_file:%s\n  output_channel(s):%d\n\n",argv[2],raw,argv[4],new);
     
@@ -72,28 +72,25 @@ int main(int argc, const char *argv[])
     
     while(1)
     {
-        if(fread(in_buf,dot_len,raw,in) != raw)
+        if(fread(in_buf,LENGTH,raw,in) != raw)
             break;
 
         for(i = 0;i < new;i++)
         {
-            if (raw == 1)
-                out_buf[i] = in_buf[0]; // synthetise
-            else
-                out_buf[i] = in_buf[i]; // cut
+            out_buf[i] = in_buf[i]; // cut
         } 
         
-        fwrite(out_buf,dot_len,new,out);
-        memset(in_buf, 0, raw*dot_len);
-        memset(out_buf, 0, new*dot_len);
+        fwrite(out_buf,LENGTH,new,out);
+        memset(in_buf, 0, raw*LENGTH);
+        memset(out_buf, 0, new*LENGTH);
     }
    
-    printf("\nSuccess\n");
-
     audio_free(in_buf);
     audio_free(out_buf);
     fclose(in);
     fclose(out);
+    
+    printf("\nSuccess\n");
     
     return 0;
 }
